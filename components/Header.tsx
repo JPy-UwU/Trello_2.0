c0272e14153978694ef8282601d9a738ae588e76
JP <jay.patel.py@gmail.com>
2023-06-08 20:50:41 -0600

    Added temp avatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Image from 'next/image';
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from 'react-avatar';
 
 const Header = () => {
   return (
@@ -12,7 +15,7 @@ const Header = () => {
           height={100}
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
         />
-        <div className="">
+        <div className="flex items-center flex-1 space-x-5 justify-end w-full">
           {/* Search Box */}
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
@@ -20,6 +23,7 @@ const Header = () => {
             <button type="submit" hidden>Search</button>
           </form>
           {/* Avatar */}
+          {/* <Avatar name="Jay Patel" round size="50" color="#0055d1" /> */}
         </div>
       </div>
     </header>
