b0c61def12d8deba49425de5af8d1e2986a5016d
JP <jay.patel.py@gmail.com>
2023-06-08 21:00:48 -0600

    Added gradient

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,9 @@ const Header = () => {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div
+          className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055d1] rounded-md filter blur-3xl opacity-50 -z-50"
+        />
         <Image 
           src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Trello-logo-blue.svg"
           alt="Trello Logo"
