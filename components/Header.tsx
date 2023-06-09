cda7d5e418022b3520438d621678d8bc272cb19e
JP <jay.patel.py@gmail.com>
2023-06-08 18:56:42 -0600

    removed assets

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,13 @@ const Header = () => {
   return (
     <header>
       <Image 
-        src="/assets/trello_logo.png"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Trello-logo-blue.svg"
         alt="Trello Logo"
         width={300}
         height={100}
         className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
       />
+
     </header>
   )
 }
